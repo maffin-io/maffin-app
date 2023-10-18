92e57b2e0f0f96e498671aadf3c0906df53366ed
Manuel Miranda <manu.mirandad@gmail.com>
2023-10-18 11:35:18 +0800

    Fix instant refresh for onboarding opening balance (#308)

--- src/components/forms/account/AccountForm.tsx
+++ src/components/forms/account/AccountForm.tsx
@@ -285,17 +285,19 @@ async function onSubmit(
 
   if (action === 'add') {
     await account.save();
+    mutate('/api/accounts');
     if (data.balance) {
       await createBalance(data, account);
     }
   } else if (action === 'update') {
     await account.save();
+    mutate('/api/accounts');
   } else if (action === 'delete') {
     await Account.remove(account);
+    mutate('/api/accounts');
     router.replace('/dashboard/accounts');
   }
 
-  mutate('/api/accounts');
   onSave(account);
 }
 
@@ -352,4 +354,5 @@ async function createBalance(data: FormValues, account: Account) {
 
   // Opening balances affect net worth
   mutate('/api/monthly-totals', undefined);
+  mutate('/api/txs/latest', undefined);
 }
