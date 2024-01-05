8da243fdadb9b4fc3f4609943f4b12cd339fe68e
argaen <manu.mirandad@gmail.com>
2024-01-05 10:32:30 +0100

    Add investment stats to account page

--- src/__tests__/components/pages/account/InvestmentInfo.test.tsx
+++ src/__tests__/components/pages/account/InvestmentInfo.test.tsx
@@ -1,26 +1,104 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { DateTime } from 'luxon';
+import type { SWRResponse } from 'swr';
 
 import { InvestmentInfo } from '@/components/pages/account';
 import InvestmentChart from '@/components/pages/account/InvestmentChart';
-import { Account } from '@/book/entities';
+import StatisticsWidget from '@/components/StatisticsWidget';
+import type { Account } from '@/book/entities';
+import Money from '@/book/Money';
+import * as apiHook from '@/hooks/api';
 
 jest.mock('@/components/pages/account/InvestmentChart', () => jest.fn(
   () => <div data-testid="InvestmentChart" />,
 ));
 
+jest.mock('@/components/StatisticsWidget', () => jest.fn(
+  () => <div data-testid="StatisticsWidget" />,
+));
+
+jest.mock('@/hooks/api', () => ({
+  __esModule: true,
+  ...jest.requireActual('@/hooks/api'),
+}));
+
+jest.mock('@/components/Loading', () => jest.fn(
+  () => <div data-testid="Loading" />,
+));
+
 describe('InvestmentInfo', () => {
-  it('renders as expected', () => {
+  beforeEach(() => {
+    jest.spyOn(apiHook, 'usePrices').mockReturnValue({ data: undefined } as SWRResponse);
+    jest.spyOn(apiHook, 'useInvestments').mockReturnValue({ data: undefined } as SWRResponse);
+  });
+
+  it('returns loading when no data', async () => {
+    render(
+      <InvestmentInfo
+        account={
+          {
+            guid: 'guid',
+            commodity: {
+              guid: 'eur_guid',
+              mnemonic: 'EUR',
+            },
+          } as Account
+        }
+      />,
+    );
+
+    await screen.findByTestId('Loading');
+  });
+
+  it('renders as expected with data', async () => {
+    const eur = {
+      guid: 'eur_guid',
+      mnemonic: 'EUR',
+    };
     const account = {
       guid: 'guid',
       commodity: {
-        guid: 'eur_guid',
-        mnemonic: 'EUR',
+        guid: 'googl_guid',
+        mnemonic: 'GOOGL',
       },
     } as Account;
+    jest.spyOn(apiHook, 'usePrices').mockReturnValue({
+      data: [
+        {
+          date: DateTime.fromISO('2023-01-01'),
+          value: 10,
+          currency: eur,
+        },
+        {
+          date: DateTime.fromISO('2023-02-01'),
+          value: 15,
+          currency: eur,
+        },
+      ],
+    } as SWRResponse);
+    jest.spyOn(apiHook, 'useInvestments').mockReturnValue({
+      data: [
+        {
+          cost: new Money(100, 'EUR'),
+          value: new Money(150, 'EUR'),
+          profitAbs: new Money(50, 'EUR'),
+          profitPct: 50,
+          avgPrice: 10,
+          quantity: new Money(10, 'GOOGL'),
+          account,
+          currency: 'EUR',
+          realizedDividends: new Money(20, 'EUR'),
+        },
+      ],
+    } as SWRResponse);
+
     const { container } = render(
       <InvestmentInfo account={account} />,
     );
+
+    await screen.findByText('10 titles');
+    await screen.findByText('€10.00');
 
     expect(InvestmentChart).toBeCalledWith(
       {
@@ -28,6 +106,40 @@ describe('InvestmentInfo', () => {
       },
       {},
     );
+    expect(StatisticsWidget).toHaveBeenNthCalledWith(
+      1,
+      {
+        className: 'col-span-6',
+        title: 'Latest known price',
+        statsTextClass: 'table-caption badge',
+        stats: '€15.00',
+        description: 'on 2/1/2023',
+      },
+      {},
+    );
+    expect(StatisticsWidget).toHaveBeenNthCalledWith(
+      2,
+      {
+        className: 'col-span-6',
+        title: 'Current value is',
+        statsTextClass: 'amount-positive',
+        stats: '€150.00',
+        description: 'from €100.00 invested',
+      },
+      {},
+    );
+    expect(StatisticsWidget).toHaveBeenNthCalledWith(
+      3,
+      {
+        className: 'col-span-8',
+        title: 'Unrealized Profit',
+        statsTextClass: 'amount-positive',
+        stats: '€50.00 (50%)',
+        description: '€70.00 (70%) with dividends',
+      },
+      {},
+    );
+
     expect(container).toMatchSnapshot();
   });
 });
--- src/components/pages/account/InvestmentInfo.tsx
+++ src/components/pages/account/InvestmentInfo.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import type { Account } from '@/book/entities';
+import { useInvestments, usePrices } from '@/hooks/api';
+import Loading from '@/components/Loading';
+import StatisticsWidget from '@/components/StatisticsWidget';
+import { toFixed } from '@/helpers/number';
+import Money from '@/book/Money';
 import InvestmentChart from './InvestmentChart';
 
 export type InvestmentInfoProps = {
@@ -10,8 +16,66 @@ export type InvestmentInfoProps = {
 export default function InvestmentInfo({
   account,
 }: InvestmentInfoProps): JSX.Element {
+  const { data: investments } = useInvestments(account.guid);
+  let { data: prices } = usePrices(account.commodity.guid);
+  const investment = investments?.[0];
+  if (!investment || !prices) {
+    return <Loading />;
+  }
+
+  prices = prices || [];
+
+  const profitAbsWithDividends = investment.profitAbs.add(investment.realizedDividends);
+  const profitPctWithDividends = (
+    profitAbsWithDividends.toNumber() / investment.cost.toNumber()
+  ) * 100;
+  const latestPrice = prices[prices.length - 1];
+
   return (
     <div className="grid grid-cols-12">
+      <div className="col-span-4">
+        <div className="card text-lg">
+          <span>You currently owe</span>
+          <span className="badge text-xl font-semibold mx-1">{`${investment.quantity.toNumber()} titles`}</span>
+          <span>at an average price of</span>
+          <span className="badge text-xl font-semibold mx-1">{new Money(investment.avgPrice, investment.currency).format()}</span>
+        </div>
+        <div className="grid grid-cols-12">
+          <StatisticsWidget
+            className="col-span-6"
+            title="Latest known price"
+            statsTextClass="table-caption badge"
+            stats={new Money(latestPrice.value, investment.currency).format()}
+            description={
+              `on ${latestPrice.date.toLocaleString()}`
+            }
+          />
+          <StatisticsWidget
+            className="col-span-6"
+            title="Current value is"
+            stats={`${investment.value.format()}`}
+            statsTextClass={classNames({
+              'amount-positive': investment.profitPct >= 0,
+              'amount-negative': investment.profitPct < 0,
+            })}
+            description={
+              `from ${investment.cost.format()} invested`
+            }
+          />
+          <StatisticsWidget
+            className="col-span-8"
+            statsTextClass={classNames({
+              'amount-positive': investment.profitPct >= 0,
+              'amount-negative': investment.profitPct < 0,
+            })}
+            title="Unrealized Profit"
+            stats={`${investment.profitAbs.format()} (${toFixed(investment.profitPct)}%)`}
+            description={
+              `${profitAbsWithDividends.format()} (${toFixed(profitPctWithDividends)}%) with dividends`
+            }
+          />
+        </div>
+      </div>
       <div className="card col-span-8">
         <InvestmentChart account={account} />
       </div>
