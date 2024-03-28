fe6787153697c499fac2d9360def2d75a4ef7b26
argaen <manu.mirandad@gmail.com>
2024-03-28 11:24:07 +0700

    Fix inconsistent totals

--- src/__tests__/hooks/api/useAccountsTotals.test.tsx
+++ src/__tests__/hooks/api/useAccountsTotals.test.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { DateTime, Interval } from 'luxon';
-import { DefinedUseQueryResult, QueryClientProvider, UseQueryResult } from '@tanstack/react-query';
-import { renderHook, waitFor } from '@testing-library/react';
+import { DefinedUseQueryResult, UseQueryResult } from '@tanstack/react-query';
+import { renderHook } from '@testing-library/react';
 
 import { useAccountsTotals } from '@/hooks/api';
 import * as incomeStatementHook from '@/hooks/api/useIncomeStatement';
@@ -25,10 +24,6 @@ jest.mock('@/hooks/state', () => ({
   ...jest.requireActual('@/hooks/state'),
 }));
 
-const wrapper = ({ children }: React.PropsWithChildren) => (
-  <QueryClientProvider client={QUERY_CLIENT}>{children}</QueryClientProvider>
-);
-
 describe('useAccountsTotals', () => {
   beforeEach(() => {
     jest.spyOn(incomeStatementHook, 'useIncomeStatement').mockReturnValue({ data: undefined } as UseQueryResult<AccountsTotals>);
@@ -38,61 +33,12 @@ describe('useAccountsTotals', () => {
 
   afterEach(() => {
     jest.clearAllMocks();
-    QUERY_CLIENT.removeQueries();
   });
 
-  it('is disabled when no balance sheet', async () => {
-    jest.spyOn(incomeStatementHook, 'useIncomeStatement').mockReturnValue({
-      data: {},
-      dataUpdatedAt: 1,
-    } as UseQueryResult<AccountsTotals>);
+  it('works when no data', async () => {
+    const { result } = renderHook(() => useAccountsTotals());
 
-    renderHook(
-      () => useAccountsTotals(),
-      { wrapper },
-    );
-
-    const queryCache = QUERY_CLIENT.getQueryCache().getAll();
-    expect(queryCache).toHaveLength(1);
-    expect(queryCache[0].queryKey).toEqual(
-      [
-        'api',
-        'splits',
-        {
-          interval: TEST_INTERVAL.toISODate(),
-          aggregation: 'total',
-          bsUpdatedAt: undefined,
-          iesUpdatedAt: 1,
-        },
-      ],
-    );
-  });
-
-  it('is disabled when no income statement', async () => {
-    jest.spyOn(balanceSheetHook, 'useBalanceSheet').mockReturnValue({
-      data: {},
-      dataUpdatedAt: 1,
-    } as UseQueryResult<AccountsTotals>);
-
-    renderHook(
-      () => useAccountsTotals(),
-      { wrapper },
-    );
-
-    const queryCache = QUERY_CLIENT.getQueryCache().getAll();
-    expect(queryCache).toHaveLength(1);
-    expect(queryCache[0].queryKey).toEqual(
-      [
-        'api',
-        'splits',
-        {
-          interval: TEST_INTERVAL.toISODate(),
-          aggregation: 'total',
-          bsUpdatedAt: 1,
-          iesUpdatedAt: undefined,
-        },
-      ],
-    );
+    expect(result.current.data).toEqual({});
   });
 
   it('merges balance sheet and income statement', async () => {
@@ -109,14 +55,10 @@ describe('useAccountsTotals', () => {
       dataUpdatedAt: 1,
     } as UseQueryResult<AccountsTotals>);
 
-    const { result } = renderHook(
-      () => useAccountsTotals(),
-      { wrapper },
-    );
+    const { result } = renderHook(() => useAccountsTotals());
 
-    await waitFor(() => expect(result.current.status).toEqual('success'));
-    expect(result.current.data?.type_asset.format()).toEqual('€10.00');
-    expect(result.current.data?.type_expense.format()).toEqual('€20.00');
+    expect(result.current.data?.type_asset.toString()).toEqual('10.00 EUR');
+    expect(result.current.data?.type_expense.toString()).toEqual('20.00 EUR');
 
     expect(balanceSheetHook.useBalanceSheet).toBeCalledWith(TEST_INTERVAL.end, undefined);
     expect(incomeStatementHook.useIncomeStatement).toBeCalledWith(TEST_INTERVAL, undefined);
@@ -128,24 +70,7 @@ describe('useAccountsTotals', () => {
       DateTime.fromISO('2023'),
     );
 
-    renderHook(
-      () => useAccountsTotals(interval),
-      { wrapper },
-    );
-
-    const queryCache = QUERY_CLIENT.getQueryCache().getAll();
-    expect(queryCache[0].queryKey).toEqual(
-      [
-        'api',
-        'splits',
-        {
-          interval: interval.toISODate(),
-          aggregation: 'total',
-          bsUpdatedAt: undefined,
-          iesUpdatedAt: undefined,
-        },
-      ],
-    );
+    renderHook(() => useAccountsTotals(interval));
 
     expect(balanceSheetHook.useBalanceSheet).toBeCalledWith(interval.end, undefined);
     expect(incomeStatementHook.useIncomeStatement).toBeCalledWith(interval, undefined);
@@ -154,10 +79,7 @@ describe('useAccountsTotals', () => {
   it('uses custom select', async () => {
     const mockSelect = jest.fn();
 
-    renderHook(
-      () => useAccountsTotals(undefined, mockSelect),
-      { wrapper },
-    );
+    renderHook(() => useAccountsTotals(undefined, mockSelect));
 
     expect(balanceSheetHook.useBalanceSheet).toBeCalledWith(TEST_INTERVAL.end, mockSelect);
     expect(incomeStatementHook.useIncomeStatement).toBeCalledWith(TEST_INTERVAL, mockSelect);
