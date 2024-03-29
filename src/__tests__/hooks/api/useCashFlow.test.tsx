885326a2626feda79e2e9764e745e0e89647a811
Manuel Miranda <manu.mirandad@gmail.com>
2024-03-29 11:53:39 +0700

    Fix multiple split (#797)

--- src/__tests__/hooks/api/useCashFlow.test.tsx
+++ src/__tests__/hooks/api/useCashFlow.test.tsx
@@ -15,7 +15,6 @@ import {
   Transaction,
 } from '@/book/entities';
 import * as stateHooks from '@/hooks/state';
-import Money from '@/book/Money';
 
 jest.mock('@/hooks/state', () => ({
   __esModule: true,
@@ -31,7 +30,6 @@ describe('useCashFlow', () => {
   let root: Account;
   let account1: Account;
   let account2: Account;
-  let tx: Transaction;
   let eur: Commodity;
 
   beforeEach(async () => {
@@ -61,33 +59,10 @@ describe('useCashFlow', () => {
 
     account2 = await Account.create({
       guid: 'guid2',
-      name: 'Bank2',
+      name: 'Expense',
       fk_commodity: eur,
       parent: root,
-      type: 'ASSET',
-    }).save();
-
-    // transfer 50 eur from bank2 to bank1
-    tx = await Transaction.create({
-      fk_currency: eur,
-      description: 'description',
-      date: DateTime.now(),
-      splits: [
-        Split.create({
-          fk_account: account1,
-          valueNum: 50,
-          valueDenom: 1,
-          quantityNum: 50,
-          quantityDenom: 1,
-        }),
-        Split.create({
-          fk_account: account2,
-          valueNum: -50,
-          valueDenom: 1,
-          quantityNum: -50,
-          quantityDenom: 1,
-        }),
-      ],
+      type: 'EXPENSE',
     }).save();
 
     jest.spyOn(stateHooks, 'useInterval').mockReturnValue({ data: TEST_INTERVAL } as DefinedUseQueryResult<Interval>);
@@ -99,6 +74,50 @@ describe('useCashFlow', () => {
   });
 
   it('calls query as expected', async () => {
+    await Transaction.create({
+      fk_currency: eur,
+      description: 'tx1',
+      date: DateTime.now(),
+      splits: [
+        Split.create({
+          fk_account: account1,
+          valueNum: -50,
+          valueDenom: 1,
+          quantityNum: -50,
+          quantityDenom: 1,
+        }),
+        Split.create({
+          fk_account: account2,
+          valueNum: 50,
+          valueDenom: 1,
+          quantityNum: 50,
+          quantityDenom: 1,
+        }),
+      ],
+    }).save();
+
+    await Transaction.create({
+      fk_currency: eur,
+      description: 'tx2',
+      date: DateTime.now(),
+      splits: [
+        Split.create({
+          fk_account: account1,
+          valueNum: -100,
+          valueDenom: 1,
+          quantityNum: -100,
+          quantityDenom: 1,
+        }),
+        Split.create({
+          fk_account: account2,
+          valueNum: 100,
+          valueDenom: 1,
+          quantityNum: 100,
+          quantityDenom: 1,
+        }),
+      ],
+    }).save();
+
     const { result } = renderHook(
       () => useCashFlow(account1.guid),
       { wrapper },
@@ -110,10 +129,11 @@ describe('useCashFlow', () => {
         guid: account2.guid,
         name: account2.name,
         type: account2.type,
-        total: expect.any(Money),
+        total: expect.objectContaining({
+          readable: '150.00 EUR',
+        }),
       },
     ]);
-    expect(result.current.data?.[0].total.toString()).toEqual('-50.00 EUR');
 
     const queryCache = QUERY_CLIENT.getQueryCache().getAll();
     expect(queryCache).toHaveLength(1);
@@ -123,6 +143,28 @@ describe('useCashFlow', () => {
   });
 
   it('uses custom interval', async () => {
+    await Transaction.create({
+      fk_currency: eur,
+      description: 'tx1',
+      date: DateTime.now(),
+      splits: [
+        Split.create({
+          fk_account: account1,
+          valueNum: -50,
+          valueDenom: 1,
+          quantityNum: -50,
+          quantityDenom: 1,
+        }),
+        Split.create({
+          fk_account: account2,
+          valueNum: 50,
+          valueDenom: 1,
+          quantityNum: 50,
+          quantityDenom: 1,
+        }),
+      ],
+    }).save();
+
     const { result } = renderHook(
       () => useCashFlow(
         account1.guid,
@@ -147,77 +189,95 @@ describe('useCashFlow', () => {
   /**
    * Given the following:
    * - one account in EUR and another in USD
-   * - first tx in EUR and second in USD
-   * - first tx sends 50 EUR to account1 and second sends 100 EUR in to account1
+   * - tx sends 50 EUR to account2
    *
    * check that when we retrieve the cash flow for
-   * account1 it creates a row  with 150 EUR referencing account2
+   * account1 it creates a row with 50 EUR referencing account2
+   *
+   * check that we retrieve the cash flow for
+   * account2 it creates a row with -55 USD referencing account1
    */
-  it('works with different commodities with account in tx currency', async () => {
+  it.each([
+    ['guid1', 'Expense', '50.00 EUR'],
+    ['guid2', 'Bank1', '-55.00 USD'],
+  ])('works with different commodities for %s account', async (guid, expectedName, expectedAmount) => {
     const usd = await Commodity.create({ mnemonic: 'USD', namespace: 'CURRENCY' }).save();
     account2.fk_commodity = usd;
     await account2.save();
 
-    tx.splits[1].quantityNum = -55;
-    await tx.save();
-
     await Transaction.create({
       fk_currency: eur,
-      description: 'tx2',
+      description: 'tx1',
       date: DateTime.now(),
       splits: [
         Split.create({
           fk_account: account1,
-          valueNum: 100,
+          valueNum: -50,
           valueDenom: 1,
-          quantityNum: 100,
+          quantityNum: -50,
           quantityDenom: 1,
         }),
         Split.create({
           fk_account: account2,
-          valueNum: -100,
+          valueNum: 50,
           valueDenom: 1,
-          quantityNum: -108,
+          quantityNum: 55,
           quantityDenom: 1,
         }),
       ],
     }).save();
 
     const { result } = renderHook(
-      () => useCashFlow(account1.guid),
+      () => useCashFlow(guid),
       { wrapper },
     );
 
     await waitFor(() => expect(result.current.status).toEqual('success'));
 
-    expect(result.current.data?.[0].total.toString()).toEqual('-150.00 EUR');
-
-    const queryCache = QUERY_CLIENT.getQueryCache().getAll();
-    expect(queryCache).toHaveLength(1);
-    expect(queryCache[0].queryKey).toEqual(
-      ['api', 'splits', 'guid1', 'cashflow', TEST_INTERVAL.toISODate()],
-    );
+    expect(result.current.data?.[0]).toMatchObject({
+      name: expectedName,
+      total: expect.objectContaining({
+        readable: expectedAmount,
+      }),
+    });
   });
 
   /**
    * Given the following:
-   *  - one account in EUR, another in USD and another in EUR
-   *  - first tx in EUR, second in USD and third in EUR
+   *  - one account in EUR, another in USD
+   *  - first tx in EUR, second in USD
    *  - first tx withdraws 55 USD from account2
    *  - second tx withdraws 108 USD from account2
-   *  - third tx adds 108 USD to account2
    *
-   * check that it creates two entries:
+   * check that it creates:
    *  - one that withdraws 163 USD referencing account1
-   *  - one that sends 108 USD referencing account3
    */
-  it('works with different commodities with account not in tx currency', async () => {
+  it('works with different txs in different currencies', async () => {
     const usd = await Commodity.create({ mnemonic: 'USD', namespace: 'CURRENCY' }).save();
     account2.fk_commodity = usd;
     await account2.save();
 
-    tx.splits[1].quantityNum = -55;
-    await tx.save();
+    await Transaction.create({
+      fk_currency: eur,
+      description: 'tx1',
+      date: DateTime.now(),
+      splits: [
+        Split.create({
+          fk_account: account1,
+          valueNum: -50,
+          valueDenom: 1,
+          quantityNum: -50,
+          quantityDenom: 1,
+        }),
+        Split.create({
+          fk_account: account2,
+          valueNum: 50,
+          valueDenom: 1,
+          quantityNum: 55,
+          quantityDenom: 1,
+        }),
+      ],
+    }).save();
 
     await Transaction.create({
       fk_currency: usd,
@@ -226,46 +286,16 @@ describe('useCashFlow', () => {
       splits: [
         Split.create({
           fk_account: account1,
-          valueNum: 100,
-          valueDenom: 1,
-          quantityNum: 100,
-          quantityDenom: 1,
-        }),
-        Split.create({
-          fk_account: account2,
-          valueNum: -100,
-          valueDenom: 1,
-          quantityNum: -108,
-          quantityDenom: 1,
-        }),
-      ],
-    }).save();
-
-    const account3 = await Account.create({
-      guid: 'guid3',
-      name: 'Bank3',
-      fk_commodity: eur,
-      parent: root,
-      type: 'ASSET',
-    }).save();
-
-    tx = await Transaction.create({
-      fk_currency: eur,
-      description: 'tx3',
-      date: DateTime.now(),
-      splits: [
-        Split.create({
-          fk_account: account2,
-          valueNum: 100,
-          valueDenom: 1,
-          quantityNum: 108,
-          quantityDenom: 1,
-        }),
-        Split.create({
-          fk_account: account3,
-          valueNum: -100,
+          valueNum: -108,
           valueDenom: 1,
           quantityNum: -100,
+          quantityDenom: 1,
+        }),
+        Split.create({
+          fk_account: account2,
+          valueNum: 108,
+          valueDenom: 1,
+          quantityNum: 108,
           quantityDenom: 1,
         }),
       ],
@@ -278,14 +308,12 @@ describe('useCashFlow', () => {
 
     await waitFor(() => expect(result.current.status).toEqual('success'));
 
-    expect(result.current.data?.[0].total.toString()).toEqual('163.00 USD');
-    expect(result.current.data?.[1].total.toString()).toEqual('-108.00 USD');
-
-    const queryCache = QUERY_CLIENT.getQueryCache().getAll();
-    expect(queryCache).toHaveLength(1);
-    expect(queryCache[0].queryKey).toEqual(
-      ['api', 'splits', 'guid2', 'cashflow', TEST_INTERVAL.toISODate()],
-    );
+    expect(result.current.data?.[0]).toMatchObject({
+      name: account1.name,
+      total: expect.objectContaining({
+        readable: '-163.00 USD',
+      }),
+    });
   });
 
   /**
@@ -303,6 +331,217 @@ describe('useCashFlow', () => {
   it('works with 3 splits in same currency', async () => {
     const account3 = await Account.create({
       guid: 'guid3',
+      name: 'Expense2',
+      fk_commodity: eur,
+      parent: root,
+      type: 'EXPENSE',
+    }).save();
+
+    await Transaction.create({
+      fk_currency: eur,
+      description: 'tx1',
+      date: DateTime.now(),
+      splits: [
+        Split.create({
+          fk_account: account1,
+          valueNum: -100,
+          valueDenom: 1,
+          quantityNum: -100,
+          quantityDenom: 1,
+        }),
+        Split.create({
+          fk_account: account2,
+          valueNum: 75,
+          valueDenom: 1,
+          quantityNum: 75,
+          quantityDenom: 1,
+        }),
+        Split.create({
+          fk_account: account3,
+          valueNum: 25,
+          valueDenom: 1,
+          quantityNum: 25,
+          quantityDenom: 1,
+        }),
+      ],
+    }).save();
+
+    const { result } = renderHook(
+      () => useCashFlow(account1.guid),
+      { wrapper },
+    );
+
+    await waitFor(() => expect(result.current.status).toEqual('success'));
+
+    expect(result.current.data?.[0]).toMatchObject({
+      name: account2.name,
+      total: expect.objectContaining({
+        readable: '75.00 EUR',
+      }),
+    });
+
+    expect(result.current.data?.[1]).toMatchObject({
+      name: account3.name,
+      total: expect.objectContaining({
+        readable: '25.00 EUR',
+      }),
+    });
+  });
+
+  /**
+   * The above case is simple however, when
+   * the accounts have different currencies, this gets a bit more complicated:
+   *
+   * - -108 USD/-100 EUR in account1
+   * - 75 EUR/75 EUR in account2
+   * - 25 EUR/25 EUR in account3
+   *
+   * Because the tx currency is in EUR, we don't know the partial amounts in USD
+   * for the other splits. We want the cashflow in USD as we are generating it for
+   * account1 so we have to calculate those amounts as:
+   *
+   *  108/100 * split.quantity
+   */
+  it('works with 3 splits in different currency', async () => {
+    const usd = await Commodity.create({ mnemonic: 'USD', namespace: 'CURRENCY' }).save();
+    account1.fk_commodity = usd;
+    await account1.save();
+
+    const account3 = await Account.create({
+      guid: 'guid3',
+      name: 'Expense2',
+      fk_commodity: eur,
+      parent: root,
+      type: 'EXPENSE',
+    }).save();
+
+    await Transaction.create({
+      fk_currency: eur,
+      description: 'tx1',
+      date: DateTime.now(),
+      splits: [
+        Split.create({
+          fk_account: account1,
+          valueNum: -100,
+          valueDenom: 1,
+          quantityNum: -108,
+          quantityDenom: 1,
+        }),
+        Split.create({
+          fk_account: account2,
+          valueNum: 75,
+          valueDenom: 1,
+          quantityNum: 75,
+          quantityDenom: 1,
+        }),
+        Split.create({
+          fk_account: account3,
+          valueNum: 25,
+          valueDenom: 1,
+          quantityNum: 25,
+          quantityDenom: 1,
+        }),
+      ],
+    }).save();
+
+    const { result } = renderHook(
+      () => useCashFlow(account1.guid),
+      { wrapper },
+    );
+
+    await waitFor(() => expect(result.current.status).toEqual('success'));
+
+    expect(result.current.data?.[0]).toMatchObject({
+      name: account2.name,
+      total: expect.objectContaining({
+        readable: '81.00 USD',
+      }),
+    });
+
+    expect(result.current.data?.[1]).toMatchObject({
+      name: account3.name,
+      total: expect.objectContaining({
+        readable: '27.00 USD',
+      }),
+    });
+  });
+
+  /**
+   * When you do mortgage payments you have a transaction
+   * to two different accounts one being a liability and another
+   * being an expense. Liability accounts also display cash flow
+   * so we want to make sure that only the split related to the
+   * liability payment is shown. For example
+   *
+   * - -1000 EUR from bank account
+   * - + 700 to liability
+   * - + 300 to expense
+   *
+   * We should only see a +300 inflow to the liability account and not
+   * +1000 and -300
+   */
+  it('works with 3 splits mortgage + interest', async () => {
+    const account3 = await Account.create({
+      guid: 'guid3',
+      name: 'Loan',
+      fk_commodity: eur,
+      parent: root,
+      type: 'LIABILITY',
+    }).save();
+
+    await Transaction.create({
+      fk_currency: eur,
+      description: 'tx1',
+      date: DateTime.now(),
+      splits: [
+        Split.create({
+          fk_account: account1,
+          valueNum: -1000,
+          valueDenom: 1,
+          quantityNum: -1000,
+          quantityDenom: 1,
+        }),
+        Split.create({
+          fk_account: account2,
+          valueNum: 300,
+          valueDenom: 1,
+          quantityNum: 300,
+          quantityDenom: 1,
+        }),
+        Split.create({
+          fk_account: account3,
+          valueNum: 700,
+          valueDenom: 1,
+          quantityNum: 700,
+          quantityDenom: 1,
+        }),
+      ],
+    }).save();
+
+    const { result } = renderHook(
+      () => useCashFlow(account3.guid),
+      { wrapper },
+    );
+
+    await waitFor(() => expect(result.current.status).toEqual('success'));
+
+    expect(result.current.data).toHaveLength(1);
+    expect(result.current.data?.[0]).toMatchObject({
+      name: account1.name,
+      total: expect.objectContaining({
+        readable: '-700.00 EUR',
+      }),
+    });
+  });
+
+  /**
+   * For tracking dividends, we add a split with 0 value/quantity
+   * referencing to the stock. However, we don't want this shown in the cash
+   * flow as it is 0 so we ignore it.
+   */
+  it('ignores 0s', async () => {
+    const account3 = await Account.create({
+      guid: 'guid3',
       name: 'Bank3',
       fk_commodity: eur,
       parent: root,
@@ -316,16 +555,16 @@ describe('useCashFlow', () => {
       splits: [
         Split.create({
           fk_account: account1,
-          valueNum: 25,
+          valueNum: 0,
           valueDenom: 1,
-          quantityNum: 25,
+          quantityNum: 0,
           quantityDenom: 1,
         }),
         Split.create({
           fk_account: account2,
-          valueNum: 75,
+          valueNum: 100,
           valueDenom: 1,
-          quantityNum: 75,
+          quantityNum: 100,
           quantityDenom: 1,
         }),
         Split.create({
@@ -345,87 +584,12 @@ describe('useCashFlow', () => {
 
     await waitFor(() => expect(result.current.status).toEqual('success'));
 
-    expect(result.current.data?.[0].name).toEqual('Bank1');
-    expect(result.current.data?.[0].total.toString()).toEqual('25.00 EUR');
-    expect(result.current.data?.[1].name).toEqual('Bank2');
-    expect(result.current.data?.[1].total.toString()).toEqual('75.00 EUR');
-
-    const queryCache = QUERY_CLIENT.getQueryCache().getAll();
-    expect(queryCache).toHaveLength(1);
-    expect(queryCache[0].queryKey).toEqual(
-      ['api', 'splits', 'guid3', 'cashflow', TEST_INTERVAL.toISODate()],
-    );
-  });
-
-  /**
-   * The above case is simple however, when
-   * the accounts have different currencies, this gets a bit more complicated:
-   *
-   * - -108 USD/-100 EUR in account1
-   * - 75 EUR/75 EUR in account2
-   * - 25 EUR/25 EUR in account3
-   *
-   * Because the tx currency is in EUR, we don't know the partial amounts in USD
-   * for the other splits. We want the cashflow in USD as we are generating it for
-   * account1 so we have to calculate those amounts as:
-   *
-   *  108/100 * split.quantity
-   */
-  it('works with 3 splits transactions with different currency', async () => {
-    const usd = await Commodity.create({ mnemonic: 'USD', namespace: 'CURRENCY' }).save();
-    const account3 = await Account.create({
-      guid: 'guid3',
-      name: 'Bank3',
-      fk_commodity: usd,
-      parent: root,
-      type: 'ASSET',
-    }).save();
-
-    await Transaction.create({
-      fk_currency: eur,
-      description: 'tx1',
-      date: DateTime.now(),
-      splits: [
-        Split.create({
-          fk_account: account1,
-          valueNum: 25,
-          valueDenom: 1,
-          quantityNum: 25,
-          quantityDenom: 1,
-        }),
-        Split.create({
-          fk_account: account2,
-          valueNum: 75,
-          valueDenom: 1,
-          quantityNum: 75,
-          quantityDenom: 1,
-        }),
-        Split.create({
-          fk_account: account3,
-          valueNum: -100,
-          valueDenom: 1,
-          quantityNum: -108,
-          quantityDenom: 1,
-        }),
-      ],
-    }).save();
-
-    const { result } = renderHook(
-      () => useCashFlow(account3.guid),
-      { wrapper },
-    );
-
-    await waitFor(() => expect(result.current.status).toEqual('success'));
-
-    expect(result.current.data?.[0].name).toEqual('Bank1');
-    expect(result.current.data?.[0].total.toString()).toEqual('27.00 USD');
-    expect(result.current.data?.[1].name).toEqual('Bank2');
-    expect(result.current.data?.[1].total.toString()).toEqual('81.00 USD');
-
-    const queryCache = QUERY_CLIENT.getQueryCache().getAll();
-    expect(queryCache).toHaveLength(1);
-    expect(queryCache[0].queryKey).toEqual(
-      ['api', 'splits', 'guid3', 'cashflow', TEST_INTERVAL.toISODate()],
-    );
+    expect(result.current.data).toHaveLength(1);
+    expect(result.current.data?.[0]).toMatchObject({
+      name: account2.name,
+      total: expect.objectContaining({
+        readable: '100.00 EUR',
+      }),
+    });
   });
 });
