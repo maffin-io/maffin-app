e235b6ad0164f79b8885c66aa300d743011df390
argaen <manu.mirandad@gmail.com>
2024-04-04 09:54:40 +0700

    Use cost for total invested

--- src/__tests__/components/pages/account/InvestmentInfo.test.tsx
+++ src/__tests__/components/pages/account/InvestmentInfo.test.tsx
@@ -209,7 +209,6 @@ describe('InvestmentInfo', () => {
     jest.spyOn(apiHook, 'useInvestment').mockReturnValue({
       data: {
         cost: new Money(100, 'EUR'),
-        totalBought: new Money(100, 'EUR'),
         value: new Money(150, 'EUR'),
         unrealizedProfitAbs: new Money(50, 'EUR'),
         unrealizedProfitPct: 50,
