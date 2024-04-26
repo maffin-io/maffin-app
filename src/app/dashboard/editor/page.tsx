d8aa498f0779d3d3f797730ce87b33d2baeccb1b
Manuel Miranda <manu.mirandad@gmail.com>
2024-04-26 15:40:43 +0700

    Basic SQL editor (#850)

---
+++ src/app/dashboard/editor/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import React from 'react';
+
+import SQLEditor from '@/components/SQLEditor';
+import SQLExplorer from '@/components/SQLExplorer';
+
+export default function EditorPage(): JSX.Element {
+  return (
+    <div className="grid grid-cols-12 items-start">
+      <div className="col-span-9">
+        <SQLEditor />
+      </div>
+      <div className="col-span-3 card">
+        <h2>Tables</h2>
+        <SQLExplorer />
+      </div>
+    </div>
+  );
+}
