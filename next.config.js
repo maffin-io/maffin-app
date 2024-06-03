30bbd015a1289cad509109d6dac51d0801292931
argaen <manu.mirandad@gmail.com>
2024-06-03 10:17:34 +0800

    Delete node modules cahce

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const CopyPlugin = require('copy-webpack-plugin');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
